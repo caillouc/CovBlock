a873f6e8ac868bf325f462707521a89cbe511f80
caillouc <cailloucolson@gmail.com>
2020-04-05 11:30:25 +0200

    exception filtered working

--- api/news.py
+++ api/news.py
@@ -12,7 +12,7 @@ CORS(app)
 ## CONSTANTES ##
 API_KEY = "f0a443efaaec4868978a2e693c779496"
 MOTS_CORONA = ["corona", "covid", "covid-19", "coronavirus",
-               "confinement", "quarantaine", "quinzaine", "wuhan", "lockdown", "quarantine", "wuhan"]
+               "confinement", "quarantaine", "quinzaine", "wuhan", "lockdown", "quarantine", "face mask"]
 
 MAX_PAGE_SIZE = 100
 
@@ -23,25 +23,34 @@ def removeEmptyEntries(newsDictionary):
     needToRemove = []
     for a in newsDictionary["articles"]:
         if(a["title"] is None or a["title"] == ''):
-            needToRemove.append(a)
+            if a not in needToRemove:
+                needToRemove.append(a)
 
     for a in needToRemove:
         newsDictionary["articles"].remove(a)
     newsDictionary["totalResults"] = len(newsDictionary["articles"])
 
 
-def removeOccurences(wordsToRemove, newsDictionary, blockingPourcentage):
-    needToRemove = []
+def isInException(exceptions, sname, sid):
+    b = False
+    for e in exceptions:
+        if(sname is not None and e in sname.lower()) or (sid is not None and e in sid.lower()):
+            b = True
+    return b
+
+
+def isInString(substring, s):
+    return s is not None and substring in s.lower()
+
+
+def removeOccurences(wordsToRemove, newsDictionary, blockingPourcentage, exception):
     for a in newsDictionary["articles"]:
         for m in wordsToRemove:
-            if ((a["title"] is not None and m in a["title"].lower()) or (a["description"] is not None and m in a["description"].lower()) or (a["url"] is not None and m in a["url"].lower()) or (a["urlToImage"] is not None and m in a["urlToImage"].lower()) or (a.get("content") is not None and m in a["content"].lower())):
-                if a not in needToRemove:
-                    needToRemove.append(a)
-    for a in needToRemove:
-        i = randrange(100)
-        if i <= blockingPourcentage:
-            newsDictionary["articles"].remove(a)
-    newsDictionary["totalResults"] = len(newsDictionary["articles"])
+            if not (isInException(exception, a["source"]["name"], a["source"]["id"])):
+                if isInString(m, a["title"]) or isInString(m, a["description"]) or isInString(m, a["url"]) or isInString(m, a["urlToImage"]) or isInString(m, a["content"]):
+                    i = randrange(100)
+                    if i <= blockingPourcentage:
+                        a["title"] = None
 
 
 def getNewsHeadlines(news_api, page_size, category, country):
@@ -53,13 +62,26 @@ def writeInFile(file, dataJson):
         json.dump(dataJson, f)
 
 
-def sendRequests(blockingPourcentage, category, country):
+def sendRequests(blockingPourcentage, category, country, exception):
     news_api = NewsApiClient(api_key=API_KEY)
-    top_headlines_no_filter = getNewsHeadlines(
-        news_api, MAX_PAGE_SIZE, category, country)
+    # top_headlines_no_filter = getNewsHeadlines(
+    #    news_api, MAX_PAGE_SIZE, category, country)
+
+    top_headlines_no_filter = {}
+    with open("init.json", "r") as f:
+        top_headlines_no_filter = json.load(f)
+    removeOccurences(MOTS_CORONA, top_headlines_no_filter,
+                     blockingPourcentage, exception)
     removeEmptyEntries(top_headlines_no_filter)
-    removeOccurences(MOTS_CORONA, top_headlines_no_filter, blockingPourcentage)
+    writeInFile("data.json", top_headlines_no_filter)
     return top_headlines_no_filter
+
+
+def splitException(exception):
+    if exception is not None:
+        return exception.split()
+    else:
+        return []
 
 
 @app.route('/news', methods=['GET', 'POST'])
@@ -69,11 +91,15 @@ def makeRequest():
         blockingPourcentage = int(request.form.get('blockingPourcentage'))
         category = request.form.get('category')
         country = request.form.get('country')
+        exception = request.form.get('exception')
+        exception = splitException(exception)
     else:
         blockingPourcentage = int(request.args.get('blockingPourcentage'))
         category = request.args.get('category')
         country = request.args.get('country')
-    return jsonify(sendRequests(blockingPourcentage, category, country))
+        exception = request.args.get('exception')
+        exception = splitException(exception)
+    return jsonify(sendRequests(blockingPourcentage, category, country, exception))
 
 
 ## MAIN ##
